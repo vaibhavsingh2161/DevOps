240a77ed1d5dabda576292ee0fb572cf680b156c
Tilak Shukla <tilakshukla233@gmail.com>
2024-09-06 17:14:48 +0530

    added sumarr

---
+++ sumarr.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+
+
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen ("output.txt", "w", stdout);
+// #endif
+
+    int arr[] = {2, 3, 4, 23, 45, 23, 23};
+    int sum = 0;
+
+    for(auto i: arr) {
+
+        sum += i;
+
+    }
+
+    cout << sum;
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}
