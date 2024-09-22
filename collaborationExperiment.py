a7bcaea95355ad698cdd0d67fe84abc437ae88f5
sankalpchiprikar <21bcs027@iiitdwd.ac.in>
2024-09-22 15:55:04 +0530

    Added Insertion Sort

--- collaborationExperiment.py
+++ collaborationExperiment.py
@@ -9,7 +9,32 @@ def bubbleSort(arr):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-    return arr  
+    return arr
+
+
+# Author: Sankalp Chiprikar
+
+def insertionSort(arr):
+    """
+    Sorts an array using the insertion sort algorithm.
+    """
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+    return arr 
+
+
+
+
+
+
+
 
 # Author: @Vaibhav Singh(21BCS128)
 def mergeSort(arr):
